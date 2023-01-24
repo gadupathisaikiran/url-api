81587faa63a785a61d453dc048c55c645a2e1c2f
Gadupathi saikiran <120211978+gadupathisaikiran@users.noreply.github.com>
2023-01-24 23:48:21 +0530

    Update url.js

--- url.js
+++ url.js
@@ -5,15 +5,29 @@ const app=express()
 
 app.get("/",async(req,res)=>{
 
+   let urlparam=req.query.url
+
    try{
-    shortUrl.short("https://docs.google.com/forms/d/e/1FAIpQLScgQ_mWibgC0MVg04iSEckI4j-dIUCZQqEVwzduw1C9SvpoYA/viewform", function (err, url) {
-        
-    res.send(url)
+    shortUrl.short(urlparam, function (err, url) {
+if(err){
+   res.send(err)
+}
+
+
+      if(url=="https://cdpt.in/rh"||!url){
+
+      res.send("<h1>enter your url in query params</h1>")
+      }
+     else{
+      res.send(`<h1>YOUR SHOTER URL=${"    "} ${url}</h1>`)
+     }
+   
+   
     });
        
     }
    catch{
-
+    
    console.log(e)
 
 
